25f9b95f594328114ce5f275d8d4d9c2fb0d1df1
KidDusty <kiddusty@gmail.com>
2020-07-19 20:22:10 -0400

    initial commit

---
+++ addATodo.js
@@ -0,0 +1,27 @@
+
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
+function inputLength(){
+	return input.value.length;
+} 
+
+function listLength(){
+	return item.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li"); 
+	li.appendChild(document.createTextNode(input.value)); 
+	ul.appendChild(li); 
+	input.value = ""; 
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) { 
+		createListElement();
+	}
+}
+enterButton.addEventListener("click",addListAfterClick);
